f96a5587bae951a2e8ce9f3c7b4b7530ec483dc6
adrienk <haloadrien@outlook.fr>
2020-11-28 22:18:12 +0100

    jsplus mtn

--- Movie-app-frontend/src/app/app.module.ts
+++ Movie-app-frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { MovieTrailerComponent } from './movies/movie/movie-trailer/movie-trailer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
+    MovieListComponent,
+    MovieComponent,
+    MovieTrailerComponent,
   ],
   imports: [
     BrowserModule,
---
+++ Movie-app-frontend/src/app/movies/movie/movie-trailer/movie-trailer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-trailer',
+  templateUrl: './movie-trailer.component.html',
+  styleUrls: ['./movie-trailer.component.css']
+})
+export class MovieTrailerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
