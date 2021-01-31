6c1b47ae082bba5971c6050d599da29d8a1f0ac9
adrienk <haloadrien@outlook.fr>
2021-01-31 19:31:55 +0100

    starting

--- Movie-app-frontend/src/app/shared/models/movie-crew.ts
+++
@@ -1,9 +0,0 @@
-export interface MovieCrew {
-  credit_id: number;
-  department: string;
-  gender: number;
-  id: number;
-  job: string;
-  name: string;
-  profile_path: string;
-}
--- Movie-app-frontend/src/app/shared/models/movie-genre.ts
+++
@@ -1,4 +0,0 @@
-export interface MovieGenre {
-  id: number;
-  name: string;
-}
